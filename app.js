09da08561e5bced08e4266f277e2185dd47df016
VladyslavPetruk96 <petrukvladyslav96@gmail.com>
2024-10-01 16:13:54 +0300

    refactoring structure

--- app.js
+++ app.js
@@ -1,15 +1,8 @@
 const express = require('express');
-const commentsRouter = require('./routes/comments');
-const userRouter = require('./routes/users');
+const router = require('./routes');
 
 const app = express();
 
-const getRootHandler = (req, res) => {
-	res.send('Get root route');
-};
-
-app.get('/', getRootHandler);
-app.use('/comments', commentsRouter);
-app.use('/users', userRouter);
+app.use(router);
 
 app.listen(4000, () => console.log('Server was started on port 4000'));
---
+++ routes/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const rootRouter = require('./root');
+const commentsRouter = require('./comments');
+const usersRouter = require('./users');
+
+const router = express.Router();
+
+router.use('/comments', commentsRouter);
+router.use('/users', usersRouter);
+router.use('/', rootRouter);
+
+module.exports = router;
